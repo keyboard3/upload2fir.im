fa88a279f09846a29101198e04ab77600d30fa98
gancy <keyboard3@icloud.com>
2018-04-23 23:15:07 +0800

    传入参数调用

--- README.md
+++ README.md
@@ -4,23 +4,11 @@ nodejs实现的apk在fir.im一键上传
 [api文档参考](https://fir.im/docs/publish)
 
 ## 操作
- - 1.配置
-	
-		let tokens = {
-			keyboard3: '自己的token'
-		}
-		let apkType = {
-			debug: 'debug/app-debug.apk',
-			preview: 'preview/app-preview.apk',
-			release: 'release/app-release.apk'
-		}
-		let activeToken = tokens.keyboard3;
-		let activeApkType = apkType.debug;
- - 4.执行
+ - 执行
 		
 		cd 项目根目录下
- 		node 存放的目录/uploadApk2fir.js
+ 		node 存放的目录/uploadApk2fir.js buildType token changeLog
 
 ## 注意
 
-此脚本自动解析git的当前的第一条日志作为changeLog、自动解析build.gradle获取applicationId,versionName,versionCode、自动解析strings.xml获取app_name
+与jenkins一起食用最佳
--- uploadApk2fir.js
+++ uploadApk2fir.js
@@ -1,104 +1,107 @@
-const fs =       require('fs');
+const fs = require('fs');
 const { exec } = require('child_process');
-var request =    require('request-promise');
+var request = require('request-promise');
 
 /* 从git从读取最新变更日志 */
 getChangeLog = ()=> {
   exec(
-    ` cd ${currentPath} 
-      && git log --pretty="%s" -1`, 
+    ` cd ${currentPath} && git log --pretty="%s" -1`, 
     (err, stdout, stderr) => {
-      err 
-      && console.error(err) 
-      && return;
-
+      if(err){
+       console.error(err);
+       return;
+      } 
       changelog = stdout;
     }
   );
 }
 /* 配置获取token的参数 */
 configFetchToken = (packageName,  activeToken) => ({
-  uri:    'http://api.fir.im/apps',
+  uri: 'http://api.fir.im/apps',
   method: 'POST',
-  headers: 
-  {
-          'Content-Type': 'application/json'
+  headers: {
+    'Content-Type': 'application/json'
   },
-  body:   `{ 
-            "type":"android", 
-            "bundle_id":"${packageName}", 
-            "api_token":"${activeToken}"
-           }`
+  body: `{ 
+          "type":"android", 
+          "bundle_id":"${packageName}", 
+          "api_token":"${activeToken}"
+         }`
 });
 /* 配置上传icon参数 */
-confifUploadIcon = cert => ({
-    method:   'POST',
-    uri:      cert.icon.upload_url,
+confifUploadIcon = (cert, icon_url) => ({
+    method: 'POST',
+    uri: cert.icon.upload_url,
     formData: 
     {
-      key:    cert.icon.key,
-      token:  cert.icon.token,
-      file:   fs.createReadStream( currentPath + '/app/src/main/res/mipmap-hdpi/ic_launcher.png' )
+      key: cert.icon.key,
+      token: cert.icon.token,
+      file: fs.createReadStream(icon_url)
     }
 });
 /* 配置上传apk文件参数 */
 configUploadfile = cert => ({
-    method:           'POST',
-    uri:              cert.binary.upload_url,
+    method: 'POST',
+    uri: cert.binary.upload_url,
     formData: 
     {
-      key:            cert.binary.key,
-      token:          cert.binary.token,
-      file:           fs.createReadStream( currentPath + `/app/build/outputs/apk/${activeApkType}` ),
-      'x:name':       appName,
-      'x:version':    version,
-      'x:build':      build,
-      'x:changelog':  changelog
+      key: cert.binary.key,
+      token: cert.binary.token,
+      file: fs.createReadStream(currentPath + `/app/build/outputs/apk/${activeApkType}.apk`),
+      'x:name': appName,
+      'x:version': version,
+      'x:build': build,
+      'x:changelog': changelog
     }
 });
 
+
+
+/*读取相关执行输入参数*/
 let currentPath = process.cwd();
-let tokens = {
-  keyboard3: '**'
+var myArgs = process.argv.slice(2);
+console.log ('myArgs',myArgs);
+
+let activeToken = myArgs[1];
+let activeApkType = myArgs[0];
+let changelog = myArgs.length > 2 ? myArgs[2]: null;
+
+if (!changelog) { getChangeLog(); }
+
+let buildConfig = fs.readFileSync(currentPath + '/app/build.gradle').toString();
+
+/* get app_name */
+let strings = fs.readFileSync(currentPath + '/app/src/main/res/values/strings.xml').toString();
+if (strings.indexOf('app_name') < 0) {
+  strings = fs.readFileSync(currentPath + `/app/build/generated/res/resValues/${activeApkType}/values/generated.xml`).toString();
 }
-let apkType = {
-  debug:     'debug/app-debug.apk',
-  preview:   'preview/app-preview.apk',
-  release:   'release/app-release.apk'
-}
-let activeToken =   tokens.keyboard3;
-let activeApkType = apkType.debug;
-let changelog =     null;
+/*get icon_url*/
+let androidManifest = fs.readFileSync(currentPath + '/app/src/main/AndroidManifest.xml').toString();
+let icon_url = `${currentPath}/app/src/main/res/mipmap-xhdpi/${androidManifest.match(/icon=".+\/(.+)"/)[1]}.png`;
 
-if (!!!changelog) { getChangeLog(); }
+let packageName = buildConfig.match(/applicationId "(\S+)"/)[1];
+let build = buildConfig.match(/versionCode (\S+)/)[1];
+let version = buildConfig.match(/versionName "(\S+)"/)[1];
+let appName = strings.match(/string name="app_name".*>(\S+)</)[1];
 
-let buildConfig = fs.readFileSync(    currentPath + '/app/build.gradle'                   ).toString();
-let strings =     fs.readFileSync(    currentPath + '/app/src/main/res/values/strings.xml').toString();
-let packageName = buildConfig.match(  /applicationId "(\S+)"/         )[1];
-let build =       buildConfig.match(  /versionCode (\S+)/             )[1];
-let version =     buildConfig.match(  /versionName "(\S+)"/           )[1];
-let appName =     strings.match(      /string name="app_name">(\S+)</ )[1];
+console.log(packageName, build, version, appName, activeToken);
 
-console.log(  packageName, build, version, appName, activeToken );
+request(configFetchToken(packageName, activeToken))
+.then(res => new Promise(resolve => resolve(JSON.parse(res))))
+.then(response => {
 
+  console.log('Upload icon beign');
+  request(confifUploadIcon(response.cert, icon_url))
+  .then(res => console.log('Upload icon successful!'))
+  .catch(err => console.error('upload icon failed:', err.message));
 
-request( configFetchToken(  packageName, activeToken  ) )
-.then( res => new Promise(  resolve => resolve( JSON.parse(res).cert ) ) )
-.then( cert => {
-
-  console.log( 'Upload icon beign' );
-  request(  confifUploadIcon(cert) )
-  .then(    res => console.log('Upload icon successful!  Server responded with:', res) )
-  .catch(   err => console.error('upload icon failed:', err.message) );
-
-  return new Promise( resolve => resolve(cert)  );
+  return new Promise(resolve => resolve(response));
 })
-.then(  cert => {
+.then(response => {
 
-  console.log( 'Upload binary beign' );
-  request( configUploadfile(cert) )
-  .then(   res => console.log('Upload binary successful!  Server responded with:', res) )
-  .catch(  err => console.error('upload binary failed:', err.message) );
-
+  console.log('Upload binary beign');
+  request(configUploadfile(response.cert))
+  .then(res => console.log('Upload binary successful!  https://fir.im/apps/'+response.id,`https://fir.im/${response.short}`))
+  .catch(err => console.error('upload binary failed:', err.message));
 })
-.catch( err => console.error(err) );
+.catch(err => console.error(err));
