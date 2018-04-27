b626606664c224743c8e91280e7330bce26d42e8
gancy <keyboard3@icloud.com>
2018-04-27 18:20:02 +0800

    支持gradle中增加后缀

--- uploadApk2fir.js
+++ uploadApk2fir.js
@@ -69,20 +69,31 @@ let changelog = myArgs.length > 2 ? myArgs[2]: null;
 if (!changelog) { getChangeLog(); }
 
 let buildConfig = fs.readFileSync(currentPath + '/app/build.gradle').toString();
-
-/* get app_name */
 let strings = fs.readFileSync(currentPath + '/app/src/main/res/values/strings.xml').toString();
-if (strings.indexOf('app_name') < 0) {
-  strings = fs.readFileSync(currentPath + `/app/build/generated/res/resValues/${activeApkType}/values/generated.xml`).toString();
-}
-/*get icon_url*/
 let androidManifest = fs.readFileSync(currentPath + '/app/src/main/AndroidManifest.xml').toString();
+/* get app_name */
+let appName = "";
+if(strings.indexOf('app_name') > 0){
+  appName = strings.match(/string name="app_name".*>(\S+)</)[1];
+}
+if (!appName) {
+  strings = fs.readFileSync(currentPath + `/app/build/generated/res/resValues/${activeApkType}/values/generated.xml`).toString();
+  appName = strings.match(/string name="app_name".*>(\S+)</)[1];
+}else {
+
+  let regexSuffix = new RegExp("^\\s+buildTypes\\s+{[\\s\\S\\n]+?\\s+"+activeApkType+"[\\s\\S\\n]+?versionNameSuffix\\s'(.+)'",'gim');
+  let matchSuffix = regexSuffix.exec(buildConfig);
+  let versionNameSuffix = matchSuffix && matchSuffix.length > 0 ? matchSuffix[1]: '';
+  appName += versionNameSuffix;
+}
+
+/*get icon_url*/
 let icon_url = `${currentPath}/app/src/main/res/mipmap-xhdpi/${androidManifest.match(/icon=".+\/(.+)"/)[1]}.png`;
 
 let packageName = buildConfig.match(/applicationId "(\S+)"/)[1];
 let build = buildConfig.match(/versionCode (\S+)/)[1];
 let version = buildConfig.match(/versionName "(\S+)"/)[1];
-let appName = strings.match(/string name="app_name".*>(\S+)</)[1];
+
 
 console.log(packageName, build, version, appName, activeToken);
 
